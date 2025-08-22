87d793775bb0b3fd3b805d184f8494e7b251bf71
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2025-08-22 16:52:18 -0700

    fix failing services

--- files/flatpak/usr/libexec/myos-hide-org-system.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-mkdir -p /var/lib/myos
-if flatpak --system remotes --columns=name | grep -qx org-system; then
-  flatpak --system remote-modify --no-enumerate org-system
-fi
-touch /var/lib/myos/org-system-hidden
